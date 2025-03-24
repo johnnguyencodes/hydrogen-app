54de5d592a4f63d047e7da739df1f0af023a44cc
John Nguyen <jnguyen92780@gmail.com>
2025-03-23 18:46:39 -0700

    #19 added vitest with demo tests

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import {defineConfig} from 'vitest/config';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom', // Set the test environment to jsdom
+    globals: true, // Enable global variables like 'describe', 'test', etc.
+    env: {},
+
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      'app/tests/end-to-end-tests/**',
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
