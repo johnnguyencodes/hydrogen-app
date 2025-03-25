c7d7aafe353df95138a3fe4c16fa9465b1de878a
John Nguyen <jnguyen92780@gmail.com>
2025-03-24 23:25:21 -0700

    playwright test debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,29 @@
-import {defineConfig, devices} from '@playwright/test';
+import {
+  defineConfig,
+  devices,
+  type PlaywrightTestConfig,
+} from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+let config: PlaywrightTestConfig = defineConfig({
   testDir: './tests/end-to-end-tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -25,11 +36,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    bypassCSP: true,
   },
 
   /* Configure projects for major browsers */
@@ -38,50 +51,49 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        extraHTTPHeaders: {
-          'Hydrogen-Bypass-Token': process.env.AUTH_BYPASS_TOKEN || '',
-        },
       },
     },
-
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        extraHTTPHeaders: {
-          'Hydrogen-Bypass-Token': process.env.AUTH_BYPASS_TOKEN || '',
-        },
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-        extraHTTPHeaders: {
-          'Hydrogen-Bypass-Token': process.env.AUTH_BYPASS_TOKEN || '',
-        },
-      },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run preview',
+    port: 3000,
+  },
 });
+
+if (process.env.URL) {
+  const use = {
+    ...config.use,
+    baseURL: process.env.URL,
+  };
+
+  if (process.env.AUTH_BYPASS_TOKEN) {
+    use.extraHTTPHeaders = {
+      'oxygen-auth-bypass-token': process.env.AUTH_BYPASS_TOKEN,
+    };
+  }
+
+  config = {
+    ...config,
+    use,
+  };
+} else {
+  config = {
+    ...config,
+    use: {
+      ...config.use,
+      baseURL: 'http://localhost:3000',
+    },
+    /* Run your local dev server before starting the tests */
+    webServer: {
+      command: 'npm run preview',
+      port: 3000,
+    },
+  };
+}
+
+export default config;
