9064fd06282c66b6ecdf78194e68994aa8c4bab7
John Nguyen <jnguyen92780@gmail.com>
2025-03-29 16:00:58 -0700

    add dynamic param support and context access

--- app/routes/plants.$handle.tsx
+++ app/routes/plants.$handle.tsx
@@ -1,9 +1,26 @@
 import {useLoaderData} from '@remix-run/react';
 import {type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 
-export async function loader(args: LoaderFunctionArgs) {
-  // this will fetch data later
-  return {};
+export async function loader({context, params}: LoaderFunctionArgs) {
+  const {handle} = params;
+  const {storefront} = context;
+
+  if (!handle) {
+    throw new Response('No handle provided', {status: 400});
+  }
+
+  const {product} = await storefront.query(PRODUCT_QUERY, {
+    variables: {
+      handle,
+      selectedOptions: [],
+    },
+  });
+
+  if (!product.id) {
+    throw new Response('Product not found', {status: 404});
+  }
+
+  return {product};
 }
 
 export default function PlantPage() {
