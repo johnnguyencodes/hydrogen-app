1b57772bc8fb780de2c36b7fe5b854c431e52325
John Nguyen <jnguyen92780@gmail.com>
2025-03-30 10:12:14 -0700

    removed server-side GTM injection from head

--- app/root.tsx
+++ app/root.tsx
@@ -149,23 +149,6 @@ export function Layout({children}: {children?: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
-        <script nonce={nonce}>
-          {`window.dataLayer = window.dataLayer || [];`}
-        </script>
-        <script
-          // START: Google Tag Manager Container
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=GTM-TGVSRM6Q'+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TGVSRM6Q');
-            `,
-          }}
-          // END: Google Tag Manager Container
-        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="stylesheet" href={tailwindCss}></link>
