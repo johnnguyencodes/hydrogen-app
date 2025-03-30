2dda410e405ea68a27514631b0ff68c05285c474
John Nguyen <jnguyen92780@gmail.com>
2025-03-29 21:23:59 -0700

    installed Google Tag Manager scripts

--- app/root.tsx
+++ app/root.tsx
@@ -149,6 +149,17 @@ export function Layout({children}: {children?: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
+        <script
+          // START: Google Tag Manager
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TGVSRM6Q');`,
+          }}
+          // END: Google Tag Manager
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="stylesheet" href={tailwindCss}></link>
@@ -158,6 +169,15 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <Links />
       </head>
       <body className="debug-screens">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGVSRM6Q"
+            height="0"
+            width="0"
+            title="Google Tag Manager NoScript"
+            style={{display: 'none', visibility: 'hidden'}}
+          ></iframe>
+        </noscript>
         {data ? (
           <Analytics.Provider
             cart={data.cart}
