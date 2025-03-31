491ae6dc196556dce935638067f2f36006b293d1
John Nguyen <jnguyen92780@gmail.com>
2025-03-30 17:26:09 -0700

    updated push to dataLayer for plant_view event

--- app/components/GoogleTagManager.tsx
+++ app/components/GoogleTagManager.tsx
@@ -14,7 +14,11 @@ export function GoogleTagManager() {
   useEffect(() => {
     subscribe('product_viewed', () => {
       // Triggering a custom event in GTM when a product is viewed
-      window.dataLayer.push({event: 'viewed-product'});
+      window.dataLayer.push({
+        event: 'plant_view',
+        plant_id: window.location.pathname, // or whatever you use
+        plant_title: 'document.title', // replace with dynamic title
+      });
     });
 
     ready();
