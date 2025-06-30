73e236f12c40c75a5f588bb877f07f2987ee80df
John Nguyen <jnguyen92780@gmail.com>
2025-06-30 08:44:10 -0700

    #135 added admin to appLoadContext in the server

--- server.ts
+++ server.ts
@@ -41,6 +41,9 @@ import {createRequestHandler} from '@shopify/remix-oxygen';
 // You define this file — it sets up your app context (Shopify client, sessions, etc.)
 import {createAppLoadContext} from '~/lib/context';
 
+// Adding Admin API client to make fetches against Shopify's admin API endpoint
+import {createAdminClient} from '~/lib/createAdminAPIClient';
+
 /**
  * Export a fetch handler in module format.
  * Shopify Oxygen looks for this `fetch()` method when serving your app.
@@ -53,12 +56,24 @@ export default {
     executionContext: ExecutionContext, // Worker execution context (background tasks)
   ): Promise<Response> {
     try {
-      // 👇 Sets up the app’s context: Shopify API client, session, provides custom logic or helpers to all Remix loaders, etc.
-      const appLoadContext = await createAppLoadContext(
+      // 👇 Sets up the app’s base context: Shopify API client, session, provides custom logic or helpers to all Remix loaders, etc.
+      const baseContext = await createAppLoadContext(
         request,
         env,
         executionContext,
       );
+
+      const {admin} = createAdminClient({
+        privateAdminToken: env.FILES_ADMIN_API_ACCESS_TOKEN,
+        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+        adminApiVerson: env.FILES_ADMIN_API_VERSION || '2025-04',
+      });
+
+      // merge the admin client into the base load context
+      const appLoadContext = {
+        ...baseContext,
+        admin,
+      };
 
       /**
        * Create the Remix request handler and pass it:
@@ -73,7 +88,7 @@ export default {
       const handleRequest = createRequestHandler({
         build: remixBuild,
         mode: process.env.NODE_ENV,
-        getLoadContext: () => appLoadContext, // makes `context` available in Remix loaders
+        getLoadContext: () => appLoadContext, // makes `context` available in Remix loaders, including `context.admin`
       });
 
       // 👇 Call Remix to handle the request (routes, loaders, rendering, etc.)
