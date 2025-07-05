e3430ebb71cb1478e45f17f121d268d9e8cbc531
John Nguyen <jnguyen92780@gmail.com>
2025-07-05 09:17:22 -0700

    #148 additional styling

--- app/routes/plants.$handle.tsx
+++ app/routes/plants.$handle.tsx
@@ -454,35 +454,35 @@ export default function Plant() {
                 <div className="journal-entries">
                   {journal.map((entry) => (
                     <div
-                      className="journal-entry bg-[var(--color-bg-5)] rounded-md p-5 mb-10"
+                      className="journal-entry bg-[var(--color-bg-5)] rounded-md p-5 mb-10 "
                       key={entry.date}
                     >
                       <div className="flex flex-col md:flex-row md:items-start gap-4">
                         <div className="flex-1">
                           <div className="mb-3 flex justify-between items-center">
                             <div className="flex items-center">
-                              <span className="text-sm font-medium text-[var(--color-text)]">
-                                {entry.date}
+                              <span className="text-sm font-medium text-[var(--color-fg-green)]">
+                                {entry.title}
                               </span>
                             </div>
                             <div className="flex items-center">
-                              <span className="text-sm font-medium text-[var(--color-text)]">
-                                32 cm
+                              <span className="text-sm font-medium text-[var(--color-fg-green)]">
+                                {entry.date}
                               </span>
                             </div>
                           </div>
                           <div
+                            className="prose prose-p:text-[var(--color-fg-text)] prose-p:text-sm text-base prose-strong:text-[var(--color-fg-green)]"
                             dangerouslySetInnerHTML={{__html: entry.content}}
                           ></div>
                         </div>
-
-                        <div className="journal-image-container flex-shrink-0 max-w-full md:max-w-md">
-                          <div className="flex gap-3 overflow-x-auto pb-2 scrollbar-hide">
+                        <div className="journal-image-container flex-shrink-0 max-w-full md:max-w-[720px]">
+                          <div className="flex gap-3 overflow-x-auto scrollbar-hide">
                             {parsedImageData.map((image, idx) =>
                               image.meta.date === entry.date &&
                               image.meta.date !== latestCarouselDateString ? (
                                 <div
-                                  className="relative rounded-md overflow-hidden border border-[var(--color-dark2)] shadow-sm flex-shrink-0 w-48 h-48"
+                                  className="overflow-hidden flex-shrink-0 w-48 h-48"
                                   key={image.image?.url ?? idx}
                                 >
                                   <ProductImage
